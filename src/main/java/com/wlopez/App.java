a068efe38594e504830fe82ecad7c2ac108dd372
wlopez11 <wlopez11@live.seminolestate.edu>
2026-02-28 16:25:22 -0500

    Change greeting to CEN4802

---
+++ src/main/java/com/wlopez/App.java
@@ -0,0 +1,12 @@
+package com.wlopez;
+
+public class App {
+
+    public static String greet(String name) {
+        return "Hello, " + name;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(greet("CEN4802"));
+    }
+}
